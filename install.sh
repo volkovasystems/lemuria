7efee2b736ad0646c6524a55ba2247d9f8c002d0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-02 10:31:23 +0800

    Update dependencies.

---
+++ install.sh
@@ -0,0 +1,20 @@
+yarn add \
+called \
+clazof \
+diatom \
+doubt \
+ethernity \
+falzy \
+harden \
+heredito \
+kein \
+llamalize \
+mongodb \
+mongoose \
+olivant \
+optfor \
+protype \
+shardize \
+titlelize \
+u200b \
+--save
--- lemuria-test.js
+++ lemuria-test.js
@@ -3,5 +3,4 @@
 const Lemuria = require( "./lemuria.js" );
 
 let UserMold = Lemuria( "User" )
-
 	.bindDatabase(  );
