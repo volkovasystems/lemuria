e8e946793456e402b9e7d20471af81267c8cfb00
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-06 14:54:00 +0800

    Updated test.

--- lemuria-test.js
+++ lemuria-test.js
@@ -3,4 +3,52 @@
 const Lemuria = require( "./lemuria.js" );
 
 let UserMold = Lemuria( "User" )
-	.bindDatabase(  );
+
+	/** Disable database connect temporarily
+		bindDatabase(  );
+
+	**/
+
+	.addFactor( "location.point" )
+
+	.addSchema( "location",
+	{
+		"point": {
+			$type: String
+		},
+
+		"area": {
+			$type: String
+		},
+
+		"region": {
+			$type: String
+		},
+
+		"country": {
+			$type: String
+		},
+
+		"name": {
+			$type: String
+		},
+
+		"title": {
+			$type: String
+		},
+
+		"address": {
+			$type: String
+		},
+
+		"latitude": {
+			$type: String
+		},
+
+		"longitude": {
+			$type: String
+		}
+
+	} );
+
+console.log( UserMold.factor );
